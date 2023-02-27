ca27e85d8c4c34813160384d7fb564245fa5bd3e
usama2333 <usama.wizz@gmail.com>
2023-02-27 17:38:58 +0500

    Redux

---
+++ src/components/Auth.js
@@ -0,0 +1,33 @@
+import { useSelector , useDispatch} from 'react-redux';
+import { authActions } from '../store/Index';
+
+import classes from './Auth.module.css';
+
+const Auth = () => {
+
+  const dispatch = useDispatch();
+
+  const loginHandler = (event) => {
+    dispatch(authActions.login());
+    event.preventDefault();
+  }
+  return (
+    <main className={classes.auth}>
+      <section>
+        <form onSubmit={loginHandler}>
+          <div className={classes.control}>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' />
+          </div>
+          <button>Login</button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default Auth;
